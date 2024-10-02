509292d3cffc57ba01cae487bea1086fed0ec2ba
A.Co <164805928+floatingparanoia@users.noreply.github.com>
2024-10-01 22:05:43 -0400

    Update models.py
    edited and updated customer class, added password functions

--- bluemoose_django/bluemoose/rewards/models.py
+++ bluemoose_django/bluemoose/rewards/models.py
@@ -3,9 +3,15 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15)  # Add this line for phone number
+    phone_number = models.CharField(max_length=15, unique=True)
+    password = models.CharField(max_length=128,default='defaultpassword' )  # To store hashed passwords
     reward_balance = models.IntegerField(default=0)
     past_purchases = models.TextField(blank=True)
+    
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
 
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
