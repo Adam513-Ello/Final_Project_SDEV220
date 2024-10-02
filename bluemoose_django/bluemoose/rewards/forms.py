e21c9faab0e6470033e08c9f4e37e62e17465361
A.Co <164805928+floatingparanoia@users.noreply.github.com>
2024-10-01 22:00:51 -0400

    Add files via upload

---
+++ bluemoose_django/bluemoose/rewards/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+class RegistrationForm(forms.Form):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    phone_number = forms.CharField(max_length=15)
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    # Custom validation to ensure passwords match
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match.")
+        
+
+class LoginForm(forms.Form):
+    phone_number = forms.CharField(max_length=15)
+    password = forms.CharField(widget=forms.PasswordInput)
