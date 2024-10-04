a661d965ccf78e0d1d77200dbb5d5f97289a3741
A.Co <164805928+floatingparanoia@users.noreply.github.com>
2024-10-03 21:17:31 -0400

    Update urls.py

--- bluemoose_django/bluemoose/rewards/urls.py
+++ bluemoose_django/bluemoose/rewards/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('success/', views.success_page, name='success_page'),
+    path('member_data/', views.member_data, name='member_data'),
+    path('logout/', views.logout_view, name='logout'),
 ]
