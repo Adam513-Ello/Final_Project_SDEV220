13734f68536a65b18913659a8047ebb20ff67c0f
A.Co <164805928+floatingparanoia@users.noreply.github.com>
2024-10-04 12:45:17 -0400

    Update admin.py

--- bluemoose_django/bluemoose/rewards/admin.py
+++ bluemoose_django/bluemoose/rewards/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Customer
+from .models import Customer, RedeemableItem, PurchasableItem, Order
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'phone_number')
+
+admin.site.register(RedeemableItem)
+admin.site.register(PurchasableItem)
+admin.site.register(Order)
+
