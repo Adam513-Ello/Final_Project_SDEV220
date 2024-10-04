1fa146c7cc5a140b33003298141cf468715fdc91
A.Co <164805928+floatingparanoia@users.noreply.github.com>
2024-10-03 21:09:36 -0400

    Update admin.py

--- bluemoose_django/bluemoose/rewards/admin.py
+++ bluemoose_django/bluemoose/rewards/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone_number')
