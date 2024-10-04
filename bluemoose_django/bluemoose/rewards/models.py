2c39394201f35a51b8d4c8b98d0a0d917a55640b
A.Co <164805928+floatingparanoia@users.noreply.github.com>
2024-10-03 21:12:51 -0400

    Update models.py

--- bluemoose_django/bluemoose/rewards/models.py
+++ bluemoose_django/bluemoose/rewards/models.py
@@ -1,17 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-class Customer(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15, unique=True)
-    password = models.CharField(max_length=128,default='defaultpassword' )  # To store hashed passwords
+class CustomerManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        if not phone_number:
+            raise ValueError('The Phone Number field must be set')
+        phone_number = self.normalize_email(phone_number)
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)  # Hashing the password
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(phone_number, password, **extra_fields)
+
+class Customer(AbstractBaseUser):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=15, unique=True)  # Ensure unique phone numbers
     reward_balance = models.IntegerField(default=0)
-    past_purchases = models.TextField(blank=True)
-    
-    def set_password(self, raw_password):
-        self.password = make_password(raw_password)
+    past_purchases = models.TextField(blank=True, null=True)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-    def check_password(self, raw_password):
-        return check_password(raw_password, self.password)
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['first_name', 'last_name']  # Add other required fields if needed
+
+    objects = CustomerManager()
+
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.phone_number
