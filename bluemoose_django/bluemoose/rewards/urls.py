e29f57c71743c640c9998bb804ee9cfefe2df86d
A.Co <164805928+floatingparanoia@users.noreply.github.com>
2024-10-04 12:44:04 -0400

    Update urls.py

--- bluemoose_django/bluemoose/rewards/urls.py
+++ bluemoose_django/bluemoose/rewards/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('success/', views.success_page, name='success_page'),
     path('member_data/', views.member_data, name='member_data'),
     path('logout/', views.logout_view, name='logout'),
+    path('redeem_item/<int:item_id>/', views.redeem_item, name='redeem_item'),
+    path('purchase_item/<int:item_id>/', views.purchase_item, name='purchase_item'),
 ]
